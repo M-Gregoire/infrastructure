0edefe6bffaa3ae496571f692666c02d02ce25bb
M-Gregoire <gregoire@martinache.net>
2019-05-27 20:42:10 +0200

    [Skuld] Rotate backups

--- nixos/hosts/Skuld/services.nix
+++ nixos/hosts/Skuld/services.nix
@@ -5,9 +5,18 @@
     description = "Backup all servers to HDD";
     serviceConfig.User = "root";
     script = ''
-      if PATH=$PATH:${pkgs.rsync}/bin:${pkgs.openssh}/bin /root/scripts/make-backup.sh; then
+      CURRENT_WEEK=$(${pkgs.coreutils}/bin/date +%V)
+      TWO_WEEKS_AGO=$(echo "($(date +%V) - 2) % 52" | ${pkgs.bc}/bin/bc)
+      if PATH=$PATH:${pkgs.rsync}/bin:${pkgs.openssh}/bin /root/scripts/make-backup.sh && \
+        mv /root/scripts/backups/last /root/scripts/backups/week-$CURRENT_WEEK; then
         echo "Backup success"
         ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backup" -F "message=Weekly backup seems to have gone smoothly!" -F "priority=5"
+        if [ -d '/root/scripts/backups/week-$TWO_WEEKS_AGO' ]; then
+          rm -rf /root/scripts/backups/week-$TWO_WEEKS_AGO
+          ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backup week-$TWO_WEEKS_AGO deleted" -F "message=The backup from two weeks ago was deleted." -F "priority=5"
+        else
+          ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backup week-$TWO_WEEKS_AGO not found" -F "message=The backup from two weeks ago couldn't be found." -F "priority=5"
+        fi
       else
         echo "Backup fail: sending notification"
         ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backuped failed" -F "message=An error occured while trying to backup the servers. Fix it!" -F "priority=5"
