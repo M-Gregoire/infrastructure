d375d670fe0f899974bea2c95c82dda131a765b5
M-Gregoire <gregoire@martinache.net>
2019-07-12 16:08:19 +0200

    [Skuld] Improve backup process

--- nixos/hosts/Skuld/services.nix
+++ nixos/hosts/Skuld/services.nix
@@ -7,12 +7,14 @@
     script = ''
       CURRENT_WEEK=$(${pkgs.coreutils}/bin/date +%V)
       TWO_WEEKS_AGO=$(echo "($(date +%V) - 2) % 52" | ${pkgs.bc}/bin/bc)
-      if PATH=$PATH:${pkgs.rsync}/bin:${pkgs.openssh}/bin /root/scripts/make-backup.sh && \
-        mv /root/scripts/backups/last /root/scripts/backups/week-$CURRENT_WEEK; then
+      BACKUP_SCRIPT=/root/scripts/Backup/make-backup.sh
+      BACKUP_FOLDER=/root/scripts/Backup/backups
+      if PATH=$PATH:${pkgs.rsync}/bin:${pkgs.openssh}/bin $BACKUP_SCRIPT && \
+        mv $BACKUP_FOLDER/last $BACKUP_FOLDER/week-$CURRENT_WEEK; then
         echo "Backup success"
         ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backup" -F "message=Weekly backup seems to have gone smoothly!" -F "priority=5"
-        if [ -d '/root/scripts/backups/week-$TWO_WEEKS_AGO' ]; then
-          rm -rf /root/scripts/backups/week-$TWO_WEEKS_AGO
+        if [ -d '$BACKUP_FOLDER/week-$TWO_WEEKS_AGO' ]; then
+          rm -rf $BACKUP_FOLDER/week-$TWO_WEEKS_AGO
           ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backup week-$TWO_WEEKS_AGO deleted" -F "message=The backup from two weeks ago was deleted." -F "priority=5"
         else
           ${pkgs.curl}/bin/curl -X POST "${config.resources.gotify.url}/message?token=${config.resources.gotify.token}" -F "title=Backup week-$TWO_WEEKS_AGO not found" -F "message=The backup from two weeks ago couldn't be found." -F "priority=5"
