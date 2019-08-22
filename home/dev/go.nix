79f955e0517fb23bef0dece6ba963ae0a479bec0
M-Gregoire <gregoire@martinache.net>
2019-08-22 16:54:35 +0200

    [PCs] Remove go-flymake

--- home/dev/go.nix
+++ home/dev/go.nix
@@ -1,28 +1,10 @@
 { pkgs, ... }:
 
-let
-
-  goflymake = pkgs.buildGoPackage rec {
-    name = "goflymake";
-    goPackagePath = "github.com/dougm/goflymake";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "dougm";
-      repo = "goflymake";
-      rev = "3b9634ef394a5ec125c6847195b1101ec1f47708";
-      sha256 = "0fy6frljzwz4y07yk602jiyk0xp83snwdsjmhk7y8akrv18vd9r3";
-    };
-  };
-
-in
-
 {
   home.packages = with pkgs;[
     go
     # Guru and other tools
     gotools
-    # Emacs flymake support
-    goflymake
     # Dependencies
     dep
   ];
