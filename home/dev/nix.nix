f3e8f23ef451cf05e33433333ae4d35acc50b210
M-Gregoire <gregoire@martinache.net>
2019-08-22 16:55:08 +0200

    [PCs] Nix utilities

--- home/dev/nix.nix
+++ home/dev/nix.nix
@@ -2,11 +2,11 @@
 
 {
   home.packages = with pkgs;[
-    #nix-prefetch-git
-    #nix-prefetch-github
-    #yarn2nix
-    #nodePackages.node2nix
-    #go2nix
+    nix-prefetch-git
+    nix-prefetch-github
+    yarn2nix
+    nodePackages.node2nix
+    go2nix
     nixops
     #cachix
   ];
