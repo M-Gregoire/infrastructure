698d0a111bab59808f976071874a36936e30b203
M-Gregoire <gregoire@martinache.net>
2019-09-01 11:50:34 +0200

    [Home] Use private home configuration

--- nixos/networks/home/default.nix
+++ nixos/networks/home/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ../../../vendor/infrastructure-private/resources/networks/home/default.nix
+  ];
+
   networking.hosts = {
     "${config.resources.hosts.beyla.ip}" = [ "Beyla" "${config.resources.hosts.beyla.alias}" ];
     "${config.resources.hosts.octopi.ip}" = [ "Octopi" "${config.resources.hosts.octopi.alias}" ];
