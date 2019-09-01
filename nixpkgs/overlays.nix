8051f8f81426e29a7e27f07e3a1d7a1a286011d1
M-Gregoire <gregoire@martinache.net>
2019-09-01 11:53:53 +0200

    [All] Use unstable docker

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -4,4 +4,11 @@
       unstable = import ~/.nix-defexpr/channels/unstable {};
     }
   )
+  (_: _: let
+    unstable = import ~/.nix-defexpr/channels/unstable { config = import ./config.nix; overlays = []; };
+  in {
+    inherit (unstable)
+      docker
+    ;
+  })
 ]
