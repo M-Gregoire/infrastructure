d93bc0d1c44d82736845791b936c5ae0c0ea8e80
M-Gregoire <gregoire@martinache.net>
2019-09-08 13:32:40 +0200

    [All] Add cleanup service

--- nixos/services.nix
+++ nixos/services.nix
@@ -9,4 +9,23 @@
   # Firewall for custom port
   networking.firewall.allowedTCPPorts = config.resources.ssh.ports;
   users.users.root.openssh.authorizedKeys.keys = config.resources.ssh.publicKeys;
+
+  systemd.services.cleanup = {
+    description = "Cleanup nix store";
+    serviceConfig.User = "root";
+    script = ''
+      nix-collect-garbage --delete-older-than 7d
+    '';
+  };
+
+  # systemctl list-timers
+
+  systemd.timers.cleanup = {
+    description = "Cleanup nix store";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "15m";
+      OnUnitInactiveSec = "1d";
+    };
+  };
 }
