53f13616f37bf9dd6794a493ad190a1d3b566e49
M-Gregoire <gregoire@martinache.net>
2020-03-06 07:48:04 +0100

    [PCs] Update apps

--- home/apps.nix
+++ home/apps.nix
@@ -50,8 +50,8 @@
     sshfs
     # Run appImage
     appimage-run
-    # Wireshark
-    wireshark
+    # Subtitles downloader
+    python37Packages.subliminal
   ];
 
   xdg.configFile."khal/config".source = builtins.toPath "${config.resources.pcs.paths.privateDotfiles}/khal/config";
