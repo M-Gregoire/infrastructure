2bc804e9372842bab9e8a5cddd48384a76caba51
M-Gregoire <gregoire@martinache.net>
2020-03-19 17:11:08 +0100

    [PCs] Add audio fix

--- home/apps.nix
+++ home/apps.nix
@@ -54,6 +54,9 @@
 
   xdg.configFile."khal/config".source = builtins.toPath "${config.resources.pcs.paths.privateDotfiles}/khal/config";
   xdg.configFile."vdirsyncer/config".source = builtins.toPath "${config.resources.pcs.paths.privateDotfiles}/vdirsyncer/config";
+  # https://unix.stackexchange.com/a/562158
+  # If encountering other sound issues, try deleting ~/.config/pulse
+  home.file.".alsoftrc".source = builtins.toPath "${config.resources.pcs.paths.publicDotfiles}/.alsoftrc";
 
   home.file.".task".source = builtins.toPath "${config.resources.pcs.paths.secrets}/task";
   programs.taskwarrior = {
