ba3488119aef369cce10adb9be3e6ea9aa2ea296
M-Gregoire <gregoire@martinache.net>
2020-04-09 11:33:20 +0200

    [Servers] Add ZSH to servers

--- nixos/profiles/Server/default.nix
+++ nixos/profiles/Server/default.nix
@@ -13,4 +13,19 @@
     # Certificate
     openssl
   ];
+
+  programs = {
+    # Needed for LightDM to remember the user
+    # See https://github.com/NixOS/nixpkgs/issues/10349#issuecomment-341810990
+    zsh = {
+      enable = true;
+      ohMyZsh.enable = true;
+      ohMyZsh.plugins = [
+        "sudo"
+        "git"
+      ];
+      ohMyZsh.theme = "agnoster";
+      enableCompletion = true;
+    };
+  };
 }
