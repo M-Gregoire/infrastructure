9729372756c406ea922861c381e091fdb3cc6a30
M-Gregoire <gregoire@martinache.net>
2020-05-02 22:45:22 +0200

    [PCs] Fix activation scripts

--- home/default.nix
+++ home/default.nix
@@ -7,24 +7,28 @@
 
   home.activation = {
     rofi = ''
-         cd ${config.resources.pcs.paths.home}/.config/rofi
-         for d in bin scripts themes launchers config.rasi; do
-           if [[ ! -L "$d" ]]; then
-              ln -s ${config.resources.pcs.paths.publicConfig}/vendor/rofi/$d $d
+         if [ -d "${config.resources.pcs.paths.home}/.config/rofi" ]; then
+           cd ${config.resources.pcs.paths.home}/.config/rofi
+           for d in bin scripts themes launchers config.rasi; do
+             if [[ ! -L "$d" ]]; then
+                ln -s ${config.resources.pcs.paths.publicConfig}/vendor/rofi/$d $d
+             fi
+           done
+           if [[ ! -L "pywal.rasi" ]]; then
+             ln -s ${config.resources.pcs.paths.home}/.cache/wal/colors-rofi-dark.rasi pywal.rasi
            fi
-         done
-         if [[ ! -L "pywal.rasi" ]]; then
-           ln -s ${config.resources.pcs.paths.home}/.cache/wal/colors-rofi-dark.rasi pywal.rasi
          fi
     '';
 
     polybar = ''
-         cd ${config.resources.pcs.paths.home}/.config/polybar
-         for d in pulseaudio-control.bash; do
-           if [[ ! -L "$d" ]]; then
-              ln -s ${config.resources.pcs.paths.publicConfig}/vendor/polybar-pulseaudio-control/$d $d
-           fi
-         done
+         if [ -d "${config.resources.pcs.paths.home}/.config/polybar" ]; then
+           cd ${config.resources.pcs.paths.home}/.config/polybar
+           for d in pulseaudio-control.bash; do
+             if [[ ! -L "$d" ]]; then
+               ln -s ${config.resources.pcs.paths.publicConfig}/vendor/polybar-pulseaudio-control/$d $d
+             fi
+           done
+         fi
     '';
 
     folders = ''
