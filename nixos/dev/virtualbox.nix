ed4b6e18abb4d32c846848fabd84aec360cdbb97
M-Gregoire <gregoire@martinache.net>
2020-06-02 17:12:31 +0200

    [Dev] Improve VirtualBox config

--- nixos/dev/virtualbox.nix
+++ nixos/dev/virtualbox.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 
 {
-  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host = {
+    enable = true;
+    enableExtensionPack = true;
+    addNetworkInterface = true;
+  };
   users.extraUsers."${config.resources.username}".extraGroups = ["vboxusers"];
 }
