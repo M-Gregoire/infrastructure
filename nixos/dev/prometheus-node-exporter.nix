ed4205ad49a078e72fee05a7e8178554190d93b9
M-Gregoire <gregoire@martinache.net>
2020-08-06 17:36:44 +0200

    [Servers] Temporary prometheus node exporter

--- nixos/dev/prometheus-node-exporter.nix
+++ nixos/dev/prometheus-node-exporter.nix
@@ -6,4 +6,8 @@
     port = 9100;
     openFirewall = true;
   };
+  # Allow access only via docker interface
+  networking.firewall.interfaces = {
+    "docker0" = { allowedTCPPorts = [ 9100 ]; };
+  };
 }
