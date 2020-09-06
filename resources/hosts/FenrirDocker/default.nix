0992aa863e8aeb203594fdb354de35c1c10313b2
M-Gregoire <gregoire@martinache.net>
2020-09-06 20:25:14 +0200

    [fenrirDocker] Set docker compose timeout

--- resources/hosts/FenrirDocker/default.nix
+++ resources/hosts/FenrirDocker/default.nix
@@ -8,4 +8,7 @@
   config.resources = with lib; mapAttrs (_: v: mkDefault v) {
     hostname = "${config.resources.hosts.fenrirDocker.hostname}";
   };
+
+  config.environment.variables."COMPOSE_HTTP_TIMEOUT" = "600";
+
 }
