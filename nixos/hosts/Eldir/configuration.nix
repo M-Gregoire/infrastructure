519ee6877679593e55b3d5b729666d29bd8d2c3e
M-Gregoire <gregoire@martinache.net>
2020-09-22 21:27:37 +0200

    [Eldir] Use systemd-networkd

--- nixos/hosts/Eldir/configuration.nix
+++ nixos/hosts/Eldir/configuration.nix
@@ -6,6 +6,7 @@
       ../../../resources/hosts/Eldir
       ../../../vendor/infrastructure-private/resources/hosts/Eldir
       ../../common.nix
+      ../../systemd-networkd.nix
       ../../networks/cloud
       ../../profiles/Server
       ./hardware-configuration.nix
@@ -33,23 +34,6 @@
   boot.initrd.network.ssh.hostRSAKey = /. + builtins.toPath "${config.resources.paths.secrets}/eldir/dropbear_rsa_host_key";
   boot.initrd.network.ssh.authorizedKeys = config.resources.services.ssh.publicKeys;
   boot.kernelParams = ["ip=:::::eth0:dhcp"];
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.dummy0.useDHCP = true;
-  networking.interfaces.erspan0.useDHCP = true;
-  networking.interfaces.eth0.useDHCP = true;
-  networking.interfaces.eth1.useDHCP = true;
-  networking.interfaces.gre0.useDHCP = true;
-  networking.interfaces.gretap0.useDHCP = true;
-  networking.interfaces.ifb0.useDHCP = true;
-  networking.interfaces.ifb1.useDHCP = true;
-  networking.interfaces.ip6tnl0.useDHCP = true;
-  networking.interfaces.sit0.useDHCP = true;
-  networking.interfaces.teql0.useDHCP = true;
-  networking.interfaces.tunl0.useDHCP = true;
 
   security.sudo.wheelNeedsPassword = false;
 
