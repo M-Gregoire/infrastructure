32c684e0d7cf0aa6f197c14963e581c5eace9bee
M-Gregoire <gregoire@martinache.net>
2020-10-17 10:33:27 +0200

    [PCs] Change screen timeout

--- scripts/xidlehook.sh
+++ scripts/xidlehook.sh
@@ -11,12 +11,12 @@ xidlehook \
   --not-when-fullscreen \
   `# Don't lock when there's audio playing` \
   --not-when-audio \
-  `# Dim the screen after 30 seconds, undim if user becomes active` \
-  --timer 30 \
+  `# Dim the screen after 45 seconds, undim if user becomes active` \
+  --timer 45 \
     'echo "1"; systemctl --user stop redshift; pkill redshift; redshift -x; redshift -l $LATITUDE:$LONGITUDE -O $CURRENT_COLOR -P -r -b $DIMMED_BRIGHTNESS:$DIMMED_BRIGHTNESS' \
     'echo "2"; redshift -x; systemctl --user start redshift' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 10 \
+  `# Undim & lock after 60 more seconds` \
+  --timer 60 \
     'echo "3"; i3lock-fancy; redshift -x; systemctl --user start redshift' \
     '' \
   `# Finally, suspend an hour after it locks` \
