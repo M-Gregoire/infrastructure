d4b9bf78c84303f5b76d6808e8c7c7f402cebf8d
M-Gregoire <gregoire@martinache.net>
2020-11-08 18:30:21 +0100

    [All] Remove unused module

--- modules/default.nix
+++ modules/default.nix
@@ -337,19 +337,6 @@
         };
       };
 
-      firefly = {
-        domain = mkOption {
-          type = types.str;
-          example = "firefly.domain.com";
-          description = "FireflyIII domain";
-        };
-        token = mkOption {
-          type = types.str;
-          example = "T0k3n";
-          description = "FireflyIII app token";
-        };
-      };
-
       git = {
         username = mkOption {
           type = types.str;
