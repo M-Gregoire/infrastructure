59d6d66370b1d9db02339b3b8199f055506427d1
M-Gregoire <gregoire@martinache.net>
2020-11-08 18:06:53 +0100

    [All] Remove unecessary overlays

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -3,8 +3,6 @@
     unstable = import ~/.nix-defexpr/channels/unstable { config = import ./config.nix; overlays = []; };
   in {
     inherit (unstable)
-      oh-my-zsh
-      nerd-fonts
     ;
   })
 ]
