46d5f6c67bbccc9324d43827db8dcabfdf73bd9c
M-Gregoire <gregoire@martinache.net>
2020-11-08 10:38:55 +0100

    [Mimir] Change GPU

--- nixos/hosts/Mimir/configuration.nix
+++ nixos/hosts/Mimir/configuration.nix
@@ -21,13 +21,6 @@
 
   networking.wireless.enable = true;
 
-  # https://github.com/NixOS/nixpkgs/issues/86212#issuecomment-640232588
-  hardware.opengl.package = (import (pkgs.fetchzip {
-    name = "old-nixpkgs";
-    url = "https://github.com/NixOS/nixpkgs/archive/0a11634a29c1c9ffe7bfa08fc234fef2ee978dbb.tar.gz";
-    sha256 = "0vj5k3djn1wlwabzff1kiiy3vs60qzzqgzjbaiwqxacbvlrci10y";
-  }) {}).mesa.drivers;
-
   networking.firewall.allowedTCPPorts = [ config.resources.hosts.mimir.ssh.port ];
   services.openssh.ports = [ config.resources.hosts.mimir.ssh.port ];
 
