e6e53e680d218f1b2fc3144d5b01c0ea33f0dc0d
M-Gregoire <gregoire@martinache.net>
2021-06-11 10:15:46 +0200

    [PCs] No displaylink

--- nixos/dev/displaylink.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
-  services.xserver.displayManager.sessionCommands = ''
-    ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
-'';
-}
