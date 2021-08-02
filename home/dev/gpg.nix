e48be0a26dfe6ca592ca6874c53f1b3d8b877048
M-Gregoire <gregoire@martinache.net>
2021-08-02 15:35:15 +0200

    [PCs] GPG Keyserver change

--- home/dev/gpg.nix
+++ home/dev/gpg.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
+
+  home.file.".gnupg/gpg.conf".source = builtins.toPath "${config.resources.paths.publicDotfiles}/gnupg/gpg.conf";
+
   home.packages = with pkgs; [pinentry-gtk2];
   services.gpg-agent = {
     enable = true;
