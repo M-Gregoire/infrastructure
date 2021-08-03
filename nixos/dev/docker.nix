94f2216a601354f4ebf26fe882070c57f53f3df5
M-Gregoire <gregoire@martinache.net>
2021-08-03 08:40:17 +0200

    [All] Docker logging

--- nixos/dev/docker.nix
+++ nixos/dev/docker.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    logDriver = "json-file";
+    extraOptions = "--log-opt max-size=10 --log-opt max-file=3";
+  };
   environment.systemPackages = with pkgs; [ docker_compose ];
 }
