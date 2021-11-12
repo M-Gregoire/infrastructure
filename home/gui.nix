e0a5db5b2ce692fd423f64c2a79cc03f8c2c350e
M-Gregoire <gregoire@martinache.net>
2021-11-12 18:54:38 +0100

    [PCs] pcmanfm improvements

--- home/gui.nix
+++ home/gui.nix
@@ -1,9 +1,20 @@
 { config, lib, pkgs, ... }:
 
-{
-  xresources.properties = {
-    "Xft.dpi" = config.resources.screen.dpi;
+let
+  # Needed for pcmanfm "Open terminal here"
+  termDesktopItem = pkgs.makeDesktopItem {
+    name = "terminal";
+    desktopName = "Kitty";
+    exec = "${pkgs.kitty}/bin/kitty -d %F";
+    icon = "kitty";
+    comment = "Fast, feature-rich, GPU based terminal";
+    genericName = "Terminal emulator";
+    categories = "System;TerminalEmulator;";
+    terminal = "false";
   };
+
+in {
+  xresources.properties = { "Xft.dpi" = config.resources.screen.dpi; };
 
   home.packages = with pkgs; [
     # Browser & emails
@@ -12,8 +23,6 @@
     thunderbird
     # Video
     mpv
-    # Torrent
-    qbittorrent
     # Ebooks
     calibre
     # Editors
@@ -27,6 +36,10 @@
     signal-desktop
     # File manager
     pcmanfm
+    termDesktopItem
+    xarchiver
+    ## Installed applications (https://nixos.wiki/wiki/PCManFM)
+    lxmenu-data
     # TODO: Check if needed
     # Network share &
     # Needed for Trash in Thunar
@@ -47,10 +60,10 @@
     element-desktop
     # Terminal emulator
     kitty
-    # Ledger
-    ledger-live-desktop
   ];
 
-  xdg.configFile."kitty/kitty.conf".source = builtins.toPath "${config.resources.paths.publicDotfiles}/kitty/kitty.conf";
-  home.file.".mozilla/firefox/${config.resources.pcs.firefox.profile}/user.js".source = builtins.toPath "${config.resources.paths.publicDotfiles}/firefox/user.js";
+  xdg.configFile."kitty/kitty.conf".source =
+    builtins.toPath "${config.resources.paths.publicDotfiles}/kitty/kitty.conf";
+  home.file.".mozilla/firefox/${config.resources.pcs.firefox.profile}/user.js".source =
+    builtins.toPath "${config.resources.paths.publicDotfiles}/firefox/user.js";
 }
