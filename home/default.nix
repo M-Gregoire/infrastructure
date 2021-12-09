d7f3e470e15d5c371aca754140bb0ada210d9680
M-Gregoire <gregoire@martinache.net>
2021-12-09 10:08:36 +0100

    [mimir] Reinstall

--- home/default.nix
+++ home/default.nix
@@ -1,40 +1,48 @@
 { config, ... }:
 
 {
-  imports = [
-    ./home.nix
-  ];
+  imports = [ ./home.nix ];
 
   home.activation = {
     rofi = ''
-         if [ -d "${config.resources.paths.home}/.config" ]; then
-           mkdir -p ${config.resources.paths.home}/.config/rofi
-           cd ${config.resources.paths.home}/.config/rofi
-           for d in bin scripts themes launchers config.rasi; do
-             if [[ ! -L "$d" ]]; then
-                ln -s ${config.resources.paths.publicConfig}/vendor/rofi/$d $d
-             fi
-           done
-           if [[ ! -L "pywal.rasi" ]]; then
-             ln -s ${config.resources.paths.home}/.cache/wal/colors-rofi-dark.rasi pywal.rasi
-           fi
-         fi
+      if [ -d "${config.resources.paths.home}/.config" ]; then
+        mkdir -p ${config.resources.paths.home}/.config/rofi
+        cd ${config.resources.paths.home}/.config/rofi
+        for d in bin scripts themes launchers config.rasi; do
+          # If link broken, delete
+          if [ ! -e "$d" ] ; then
+            rm $d
+          fi
+          if [[ ! -L "$d" ]]; then
+             ln -s ${config.resources.paths.publicConfig}/vendor/rofi/$d $d
+          fi
+        done
+        if [ ! -e "$d" ] ; then
+           rm $d
+        fi
+        if [[ ! -L "pywal.rasi" ]]; then
+            ln -s ${config.resources.paths.home}/.cache/wal/colors-rofi-dark.rasi pywal.rasi
+        fi
+      fi
     '';
 
     polybar = ''
-         if [ -d "${config.resources.paths.home}/.config/polybar" ]; then
-           cd ${config.resources.paths.home}/.config/polybar
-           for d in pulseaudio-control.bash; do
-             if [[ ! -L "$d" ]]; then
-               ln -s ${config.resources.paths.publicConfig}/vendor/polybar-pulseaudio-control/$d $d
-             fi
-           done
-         fi
+      if [ -d "${config.resources.paths.home}/.config/polybar" ]; then
+        cd ${config.resources.paths.home}/.config/polybar
+        for d in pulseaudio-control.bash; do
+          if [ ! -e "$d" ] ; then
+            rm $d
+          fi
+          if [[ ! -L "$d" ]]; then
+            ln -s ${config.resources.paths.publicConfig}/vendor/polybar-pulseaudio-control/$d $d
+          fi
+        done
+      fi
     '';
 
     folders = ''
-         mkdir -p ${config.resources.paths.home}/src/github.com/${config.resources.services.git.username}
-         mkdir -p ${config.resources.paths.home}/Screenshots/
+      mkdir -p ${config.resources.paths.home}/src/
+      mkdir -p ${config.resources.paths.home}/Screenshots/
     '';
   };
 }
--- resources/hosts/mimir/default.nix
+++ resources/hosts/mimir/default.nix
@@ -1,17 +1,15 @@
-{ config, lib,... }:
+{ config, lib, ... }:
 
 {
-  imports = [
-    ../../common.nix
-    ../../profiles/PC
-  ];
+  imports = [ ../../common.nix ../../profiles/PC ];
 
-  config.resources = with lib; mapAttrs (_: v: mkDefault v) {
-    hostname = "${config.resources.hosts.mimir.hostname}";
-    luks.drive = "/dev/disk/by-uuid/c734259d-b1a8-449e-a991-ace9d6f561ed";
-    screen = {
-      dpi = "96";
-      scaleFactor = "1";
+  config.resources = with lib;
+    mapAttrs (_: v: mkDefault v) {
+      hostname = "${config.resources.hosts.mimir.hostname}";
+      luks.drive = "/dev/disk/by-uuid/74bd5c5b-7619-4d5e-8886-958fdbc8e90d";
+      screen = {
+        dpi = "96";
+        scaleFactor = "1";
+      };
     };
-  };
 }
