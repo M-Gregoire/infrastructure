5c24420a3cf56ffd10fbcf36cb378e1a091f0b76
M-Gregoire <gregoire@martinache.net>
2022-05-29 09:42:50 +0200

    [PCs] Rofi scripts update

--- home/default.nix
+++ home/default.nix
@@ -8,21 +8,26 @@
       if [ -d "${config.resources.paths.home}/.config" ]; then
         mkdir -p ${config.resources.paths.home}/.config/rofi
         cd ${config.resources.paths.home}/.config/rofi
-        for d in bin scripts themes launchers config.rasi; do
+        for d in *; do
           # If link broken, delete
           if [ ! -e "$d" ] ; then
             rm -f $d
           fi
-          if [[ ! -L "$d" ]]; then
-             ln -s ${config.resources.paths.publicConfig}/vendor/rofi/$d $d
+	done
+        for d in ${config.resources.paths.publicConfig}/vendor/rofi/1080p/*; do
+	  f=$(basename "$d")
+          if [[ ! -L "$f" ]]; then
+             ln -s ${config.resources.paths.publicConfig}/vendor/rofi/1080p/$f $f
           fi
         done
-        if [ ! -e "$d" ] ; then
-           rm -f $d
-        fi
         if [[ ! -L "pywal.rasi" ]]; then
             ln -s ${config.resources.paths.home}/.cache/wal/colors-rofi-dark.rasi pywal.rasi
         fi
+	# Modify the scripts to get all parameters from environment variables
+	for val in "theme" "color" "shutdown" "reboot" "lock" "suspend" "logout"; do
+	    sed -i "s/^$val/#$val/" ${config.resources.paths.publicConfig}/vendor/rofi/1080p/powermenu/powermenu.sh
+	    sed -i "s/^$val/#$val/" ${config.resources.paths.publicConfig}/vendor/rofi/1080p/launchers/ribbon/launcher.sh
+	done
       fi
     '';
 
