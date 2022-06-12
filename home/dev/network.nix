b96b989145bedf84e6ffec0f45e5211134a5182f
M-Gregoire <gregoire@martinache.net>
2022-06-12 16:17:46 +0200

    [PCs] Wavemon

--- home/dev/network.nix
+++ home/dev/network.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs;[
+  home.packages = with pkgs; [
     # OpenSSL
     openssl
     # Wireless tools like iwgetid
     wirelesstools
+    # TUI Wifi scanner
+    wavemon
   ];
 
 }
