19043f3ebc88841c19fe477c0d16ba54c9ecf845
M-Gregoire <gregoire@martinache.net>
2022-06-25 13:12:00 +0200

    [Kvasir] Huawei E3531 -> 3G Mode

--- nixos/hosts/kvasir/configuration.nix
+++ nixos/hosts/kvasir/configuration.nix
@@ -20,5 +20,16 @@
   system.stateVersion = "20.03";
   networking.firewall.enable = false;
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
   boot.kernelModules = [ "rdb" "ceph" ];
+
+  environment.systemPackages = [ pkgs.usb_modeswitch ];
+  services.udev.extraRules = ''
+    # If Modem in USB Storage mode, switch to 3G mode
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="12d1", ATTRS{idProduct}=="15ca", RUN+="${pkgs.usb_modeswitch}/bin/usb_modeswitch -v 12d1 -p 15ca -M '55534243123456780000000000000011062000000100000000000000000000'"
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="12d1", ATTRS{idProduct}=="1506", RUN+="${pkgs.bash}/bin/bash -c 'modprobe option && echo 12d1 1506 > /sys/bus/usb-serial/drivers/option1/new_id'"
+
+    # If Modem in 3G mode, make alias named ttyUSB-3G
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="12d1", ATTRS{idProduct}=="1506", SYMLINK+="ttyUSB-3G"
+  '';
 }
