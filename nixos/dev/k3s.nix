0d00c134013187405772617f3d2c23f20c113b56
M-Gregoire <gregoire@martinache.net>
2023-01-26 09:50:58 +0100

    feat: Fix k3s node pressure

--- nixos/dev/k3s.nix
+++ nixos/dev/k3s.nix
@@ -3,6 +3,11 @@
 {
   services.k3s.enable = true;
   services.k3s.role = "server";
+
+  # TODO: Investigate why DiskPressure without this
+  services.k3s.extraFlags =
+    "--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1% --kubelet-arg=eviction-minimum-reclaim=imagefs.available=1%,nodefs.available=1%";
+
   environment.systemPackages = [ pkgs.k3s pkgs.containerd pkgs.kubectl ];
   networking.firewall.allowedTCPPorts = [ 6443 ];
 }
