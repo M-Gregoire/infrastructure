701bb450f4f7e50f45557b61dca1077386a6f5a8
M-Gregoire <gregoire@martinache.net>
2023-05-07 16:17:08 +0200

    feat: Move pipewire to stable

--- nixos/dev/audio.nix
+++ nixos/dev/audio.nix
@@ -1,11 +1,11 @@
 { pkgs, config, ... }:
 
 {
-  disabledModules = [ "services/desktops/pipewire/pipewire.nix" ];
-  imports = [ # Use service from nixos-unstable channel.
-    # sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-    <nixos-unstable/nixos/modules/services/desktops/pipewire/pipewire.nix>
-  ];
+  # disabledModules = [ "services/desktops/pipewire/pipewire.nix" ];
+  # imports = [ # Use service from nixos-unstable channel.
+  #   # sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+  #   <nixos-unstable/nixos/modules/services/desktops/pipewire/pipewire.nix>
+  # ];
 
   # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
   sound.enable = false;
