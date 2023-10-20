68f4b8f41bdf93e8c9f96eb305dc9d2100bc014d
M-Gregoire <gregoire@martinache.net>
2023-10-20 10:36:45 +0200

    feat: Update channels to 23.05

--- scripts/setChannels.sh
+++ scripts/setChannels.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-cat << EOF > ~/.nix-channels
-https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
-https://nixos.org/channels/nixos-22.11 nixpkgs
-https://nixos.org/channels/nixos-unstable nixpkgs-unstable
-EOF
-
 sudo bash -c "cat << EOF > ~/.nix-channels
-https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
-https://nixos.org/channels/nixos-22.11 nixos
+https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+https://nixos.org/channels/nixos-23.05 nixos
 https://nixos.org/channels/nixos-unstable nixos-unstable
 https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
 EOF"
+
+cat << EOF > ~/.nix-channels
+https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+https://nixos.org/channels/nixos-23.05 nixpkgs
+https://nixos.org/channels/nixos-unstable nixpkgs-unstable
+EOF
+
+nix-channel --update && sudo nix-channel --update
