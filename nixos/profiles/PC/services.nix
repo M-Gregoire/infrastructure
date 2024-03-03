66eb6fee6ea07877f09c14efd84dccbf5d62548c
M-Gregoire <gregoire@martinache.net>
2024-03-03 13:31:27 +0100

    feat: Update nextcloudpath

--- nixos/profiles/PC/services.nix
+++ nixos/profiles/PC/services.nix
@@ -50,7 +50,7 @@
         Type = "simple";
         # TODO: Remove Grégoire
         ExecStart =
-          "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --path /Grégoire/org ${config.resources.paths.home}/org ${config.resources.services.nextcloud.url}";
+          "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --path /org ${config.resources.paths.home}/org ${config.resources.services.nextcloud.url}";
         TimeoutStopSec = "180";
         # KillMode = "process";
         # KillSignal = "SIGINT";
