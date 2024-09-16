36ed3e162e862e91a39248db1a3f4d2c32995bde
M-Gregoire <gregoire@martinache.net>
2024-09-16 15:16:08 +0200

    fix: Spicetify

--- home/dev/linux/spicetify.nix
+++ home/dev/linux/spicetify.nix
@@ -1,23 +1,23 @@
 { pkgs, lib, spicetify-nix, ... }:
-let spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
+
+let spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
 in {
   # allow spotify to be installed if you don't have unfree enabled already
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "spotify" ];
 
   # import the flake's module for your system
-  imports = [ spicetify-nix.homeManagerModule ];
+  imports = [ spicetify-nix.homeManagerModules.default ];
 
   # configure spicetify :)
   programs.spicetify = {
     enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      hidePodcasts
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+    ];
     theme = spicePkgs.themes.catppuccin;
     colorScheme = "mocha";
-
-    enabledExtensions = with spicePkgs.extensions; [
-      fullAppDisplay
-      shuffle # shuffle+ (special characters are sanitized out of ext names)
-      hidePodcasts
-    ];
   };
 }
