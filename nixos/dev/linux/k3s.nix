c40dacac3b870e7279a22d05fbb5ff2cd87efbad
M-Gregoire <gregoire@martinache.net>
2024-12-10 10:44:26 +0100

    fix: Certificate generation on k3s

--- nixos/dev/linux/k3s.nix
+++ nixos/dev/linux/k3s.nix
@@ -6,7 +6,7 @@ with lib; {
 
     # TODO: Investigate why DiskPressure without this
     services.k3s.extraFlags =
-      "--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1% --kubelet-arg=eviction-minimum-reclaim=imagefs.available=1%,nodefs.available=1%";
+      "--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1% --kubelet-arg=eviction-minimum-reclaim=imagefs.available=1%,nodefs.available=1% --kubelet-arg=runtime-request-timeout=5m0s --tls-san ${config.resources.hostname}.${config.resources.networking.domain}";
 
     environment.systemPackages = [ pkgs.k3s pkgs.containerd pkgs.kubectl ];
     networking.firewall.allowedTCPPorts = [ 6443 ];
