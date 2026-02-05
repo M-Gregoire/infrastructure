8ef968a62d8903edb91e3d7e006a7928b89aecbb
M-Gregoire <gregoire.cadenemartinache@datadoghq.com>
2026-02-05 09:59:05 +0100

    feat: Dynamically get Github Token

--- machines/default.nix
+++ machines/default.nix
@@ -4,8 +4,9 @@
 {
 
   config = {
-
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
     environment.systemPackages = with pkgs; [
       # File type
       file
@@ -35,10 +36,29 @@
       enable = true;
 
       interactiveShellInit = ''
-        # Only set aliases in interactive shells
-          alias cd='z'
-          alias cat='bat'
-          alias ls='eza'
+                # Only set aliases in interactive shells
+                  alias cd='z'
+                  alias cat='bat'
+                  alias ls='eza'
+
+
+        load-github-token() {
+            [ -n "$GITHUB_TOKEN" ] && return
+
+            HOSTNAME=$(hostname)
+
+            if [ "$HOSTNAME" = "COMP-CQ5H77T0CQ" ]; then
+                export GITHUB_TOKEN=$(op read "op://Employee/GitHub/dnq26vcp54uun55gryg6pplmmm")
+            else
+                export GITHUB_TOKEN=$(bw get password github-nix-token)
+            fi
+        }
+
+        # Auto-load token for nix commands
+        nix() {
+            load-github-token
+            command nix --access-tokens "github.com=$GITHUB_TOKEN" "$@"
+        }
       '';
     };
 
