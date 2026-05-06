1835c51c895a0cde6c2ac5859bc664592fbb4d20
M-Gregoire <gregoire.cadenemartinache@datadoghq.com>
2026-05-06 13:26:04 +0200

    Remove pi-oauth: replaced by atlassian.ts pi extension
    - Remove pi-oauth.py (Python script that proxied Atlassian/Slack via MCP)
    - Remove Nix deployment from datadog host
    - Jira/Confluence now available as native pi tools via ~/.pi/agent/extensions/atlassian.ts
    - Slack already covered by slack-mcp extension

--- home/hosts/datadog/default.nix
+++ home/hosts/datadog/default.nix
@@ -3,7 +3,5 @@
 {
   home.packages = with pkgs; [
     jira-cli-go
-    (writers.writePython3Bin "pi-oauth" { flakeIgnore = [ "E265" ]; }
-      (builtins.readFile "${flake-root}/dotfiles/pi-oauth.py"))
   ];
 }
