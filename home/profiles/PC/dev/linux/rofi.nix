3c57befc4f42a1ed636692eb1e184ea156b2604c
M-Gregoire <gregoire.cadenemartinache@datadoghq.com>
2026-06-15 16:46:34 +0200

    fix(rofi): copy mutable vendor dirs, symlink the rest, fix nix store write errors

--- home/profiles/PC/dev/linux/rofi.nix
+++ home/profiles/PC/dev/linux/rofi.nix
@@ -16,20 +16,26 @@
             rm -f $d
           fi
         done
+        # Dirs that need local modifications (can't be nix store symlinks)
+        for d in applets launchers powermenu; do
+          if [ -L "$d" ]; then rm "$d"; fi
+          if [ ! -d "$d" ]; then
+            cp -r ${flake-root}/vendor/rofi/files/$d $d
+            chmod -R u+w $d
+          fi
+        done
+        # Everything else can be symlinked
         for d in ${flake-root}/vendor/rofi/files/*; do
           f=$(basename "$d")
-          ln -sfn ${flake-root}/vendor/rofi/files/$f $f
+          case "$f" in applets|launchers|powermenu) continue;; esac
+          ln -sfn "$d" "$f"
         done
         rm -rf pywal.rasi
         ln -s $HOME/.cache/wal/colors-rofi-dark.rasi pywal.rasi
         # Set theme
-        rm $HOME/.config/rofi/powermenu/type-2/shared/colors.rasi
         echo '@import "~/.config/rofi/colors/solarized.rasi"' > $HOME/.config/rofi/powermenu/type-2/shared/colors.rasi
-        rm $HOME/.config/rofi/launchers/type-2/shared/colors.rasi
         echo '@import "~/.config/rofi/colors/solarized.rasi"' > $HOME/.config/rofi/launchers/type-2/shared/colors.rasi
-        rm $HOME/.config/rofi/applets/shared/colors.rasi
         echo '@import "~/.config/rofi/colors/solarized.rasi"' > $HOME/.config/rofi/applets/shared/colors.rasi
-        rm $HOME/.config/rofi/applets/shared/theme.bash
         echo 'type="$HOME/.config/rofi/applets/type-2"' > $HOME/.config/rofi/applets/shared/theme.bash
         echo "style='style-1.rasi'" >> $HOME/.config/rofi/applets/shared/theme.bash
       fi
