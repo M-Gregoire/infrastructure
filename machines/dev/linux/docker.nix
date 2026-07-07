a363754af08b16cee46ede7b16b844a1e4747ef1
Grégoire M. <gregoire.cadenemartinache@datadoghq.com>
2026-07-07 14:22:25 +0200

    docker: fallback to default package if docker_28 not available

--- machines/dev/linux/docker.nix
+++ machines/dev/linux/docker.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   virtualisation.docker = {
     enable = true;
-    package = pkgs.docker_28;
+    package = if pkgs ? docker_28 then pkgs.docker_28 else pkgs.docker;
     logDriver = "json-file";
     extraOptions = "--log-opt max-size=10 --log-opt max-file=3";
   };
