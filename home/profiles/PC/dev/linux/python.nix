896002a008dc1c6040320e3c564e4dae08e72697
Grégoire M. <gregoire.cadenemartinache@datadoghq.com>
2026-07-07 16:10:53 +0200

    python: use nixpkgs pywalfox-native instead of custom derivation

--- home/profiles/PC/dev/linux/python.nix
+++ home/profiles/PC/dev/linux/python.nix
@@ -1,22 +1,9 @@
 { pkgs, ... }:
 
-let
-
-  pywalfox = pkgs.python314.pkgs.buildPythonPackage {
-    pname = "pywalfox";
-    version = "2.8.0rc1";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "Frewacom";
-      repo = "pywalfox-native";
-      rev = "7ecbbb193e6a7dab424bf3128adfa7e2d0fa6ff9";
-      hash = "sha256-i1DgdYmNVvG+mZiFiBmVHsQnFvfDFOFTGf0GEy81lpE=";
-    };
-  };
-in {
+{
   home.packages = with pkgs;
     [
-      pywalfox
+      pywalfox-native
       # TODO: Restore python packages
       (python314.withPackages (ps:
         with ps;
