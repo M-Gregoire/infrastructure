e82eaeefe299f4c8702bb6376850b88a34f2d31a
Grégoire M. <gregoire.cadenemartinache@datadoghq.com>
2026-07-07 14:22:52 +0200

    docker: use default package, no version pin

--- machines/dev/linux/docker.nix
+++ machines/dev/linux/docker.nix
@@ -1,9 +1,8 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   virtualisation.docker = {
     enable = true;
-    package = if pkgs ? docker_28 then pkgs.docker_28 else pkgs.docker;
     logDriver = "json-file";
     extraOptions = "--log-opt max-size=10 --log-opt max-file=3";
   };
