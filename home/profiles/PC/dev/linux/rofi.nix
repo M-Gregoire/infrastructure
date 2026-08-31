c143ada56bcaf77e30583aae2214f3551646f365
M-Gregoire <gregoire@martinache.net>
2026-08-31 20:52:02 +0200

    chore: manage rofi config declaratively

--- home/profiles/PC/dev/linux/rofi.nix
+++ home/profiles/PC/dev/linux/rofi.nix
@@ -1,45 +1,40 @@
-{ config, lib, pkgs, flake-root, ... }:
-
 {
-  home.activation = {
-    # I'm using rofi themes from https://github.com/adi1090x/rofi
-    # But it needs a few changes to be able to select icons and theme from environment variables alone
-    # This script patches it
-    # TODO: This is sub-optimal as the vendor directory is modified and triggers a git change.
-    rofi = ''
-      if [ -d "${config.resources.paths.home}/.config" ]; then
-        mkdir -p ${config.resources.paths.home}/.config/rofi
-        cd ${config.resources.paths.home}/.config/rofi
-        for d in *; do
-          # If link broken, delete
-          if [ ! -e "$d" ] ; then
-            rm -f $d
-          fi
-        done
-        # Dirs that need local modifications (can't be nix store symlinks)
-        for d in applets launchers powermenu; do
-          if [ -L "$d" ]; then rm "$d"; fi
-          if [ ! -d "$d" ]; then
-            cp -r ${flake-root}/vendor/rofi/files/$d $d
-            chmod -R u+w $d
-          fi
-        done
-        # Everything else can be symlinked
-        for d in ${flake-root}/vendor/rofi/files/*; do
-          f=$(basename "$d")
-          case "$f" in applets|launchers|powermenu) continue;; esac
-          ln -sfn "$d" "$f"
-        done
-        rm -rf pywal.rasi
-        ln -s $HOME/.cache/wal/colors-rofi-dark.rasi pywal.rasi
-        # Set theme
-        echo '@import "~/.config/rofi/colors/solarized.rasi"' > $HOME/.config/rofi/powermenu/type-2/shared/colors.rasi
-        echo '@import "~/.config/rofi/colors/solarized.rasi"' > $HOME/.config/rofi/launchers/type-2/shared/colors.rasi
-        echo '@import "~/.config/rofi/colors/solarized.rasi"' > $HOME/.config/rofi/applets/shared/colors.rasi
-        echo 'type="$HOME/.config/rofi/applets/type-2"' > $HOME/.config/rofi/applets/shared/theme.bash
-        echo "style='style-1.rasi'" >> $HOME/.config/rofi/applets/shared/theme.bash
-      fi
-    '';
+  config,
+  pkgs,
+  flake-root,
+  ...
+}:
 
-  };
+let
+  rofiConfig = pkgs.runCommand "rofi-config" { } ''
+    cp -R ${flake-root}/vendor/rofi/files "$out"
+    chmod -R u+w "$out"
+
+    printf '%s\n' '@import "~/.config/rofi/colors/solarized.rasi"' \
+      > "$out/powermenu/type-2/shared/colors.rasi"
+    printf '%s\n' '@import "~/.config/rofi/colors/solarized.rasi"' \
+      > "$out/launchers/type-2/shared/colors.rasi"
+    printf '%s\n' '@import "~/.config/rofi/colors/solarized.rasi"' \
+      > "$out/applets/shared/colors.rasi"
+    printf '%s\n' 'type="$HOME/.config/rofi/applets/type-2"' "style='style-1.rasi'" \
+      > "$out/applets/shared/theme.bash"
+
+    # Preserve the previous pywal.rasi entry point without needing a mutable
+    # symlink inside ~/.config/rofi.
+    printf '%s\n' '@import "~/.cache/wal/colors-rofi-dark.rasi"' \
+      > "$out/pywal.rasi"
+  '';
+in
+{
+  # The old activation script created ~/.config/rofi as a mutable directory.
+  # Back it up once so Home Manager can replace it with a managed symlink.
+  home.activation.cleanupMutableRofiConfig = config.lib.dag.entryBefore [ "checkLinkTargets" ] ''
+    if [ -e "$HOME/.config/rofi" ] && [ ! -L "$HOME/.config/rofi" ]; then
+      backup="$HOME/.config/rofi.pre-nix-store.$(date +%Y%m%d%H%M%S)"
+      echo "Moving existing mutable rofi config to $backup"
+      $DRY_RUN_CMD mv "$HOME/.config/rofi" "$backup"
+    fi
+  '';
+
+  xdg.configFile."rofi".source = rofiConfig;
 }
